70b6dc607ca2eef34112dd521cef090c0d6aa3aa
s-loar <sjl7909@gmail.com>
2023-01-05 16:11:19 -0500

    Add redirect, if logged in, from root to /guess

--- lib/pento_web/controllers/page_controller.ex
+++ lib/pento_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule PentoWeb.PageController do
   use PentoWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn = PentoWeb.UserAuth.fetch_current_user(conn, [])
+    if conn.assigns[:current_user] do
+      redirect(conn, to: "/guess")
+    else
+      render(conn, "index.html")
+    end
   end
 end
