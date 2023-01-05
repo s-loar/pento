b66613a0d039327778e27474876e0c34cff29978
s-loar <sjl7909@gmail.com>
2023-01-05 14:15:12 -0500

    Add username to users and make unique

---
+++ priv/repo/migrations/20230105184614_users_add_username_column.exs
@@ -0,0 +1,10 @@
+defmodule Pento.Repo.Migrations.UsersAddUsernameColumn do
+  use Ecto.Migration
+
+  def change do
+    alter table("users") do
+      add :username, :string
+    end
+    create unique_index(:users, [:username])
+  end
+end
