7771a9435312ad993cd245148236c817623fb9da
s-loar <sjl7909@gmail.com>
2023-01-05 17:02:06 -0500

    Refactor to use assign_new to reduce the DB calls

--- lib/pento_web/live/user_auth_live.ex
+++ lib/pento_web/live/user_auth_live.ex
@@ -6,7 +6,7 @@ defmodule PentoWeb.UserAuthLive do
   def on_mount(_, params, %{"user_token" => user_token} = _session, socket) do
     socket =
     socket
-    |> assign(:current_user, Accounts.get_user_by_session_token(user_token))
+    |> assign_new(:current_user, fn -> Accounts.get_user_by_session_token(user_token) end)
 
     IO.inspect(socket.assigns.current_user)
     if socket.assigns.current_user do
